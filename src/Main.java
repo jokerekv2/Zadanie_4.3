4f634deae13752c4df2c5ec0b9980e9a082d0448
Dominik Mikolajczyk <jokerekv2@gmail.com>
2020-10-13 23:30:28 +0200

    rozwiązanie

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,15 @@
 public class Main {
+    public static void main(String[] args) {
+        Task task1 = new Task("zakupy", "kupić mleko",
+                new Person("Dominik", "Mikolajczyk"), 1 );
+        Task task2 = new Task("gitara", "nastroić gitarę", 0 );
+        Task task3 = new Task("obiad", "naleśniki");
+        Task task4 = new Task("lololo", "tralala", -1 );
 
+
+        System.out.println(task1.highPriority());
+        System.out.println(task2.mediumPriority());
+        System.out.println(task3.lowPriority());
+        System.out.println(task4.lowPriority());
+    }
 }
--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,74 @@
 public class Task {
+    private String name;
+    private String description;
+    private Person person;
+    private int priority;
 
+    public Task(String name, String description, Person person, int priority) {
+        this(name, description, priority);
+        this.person = person;
+    }
+
+    public Task(String name, String description, int priority) {
+        this(name, description);
+        this.priority = priority;
+    }
+
+    public Task(String name, String description) {
+        this.name = name;
+        this.description = description;
+    }
+
+    public boolean highPriority() {
+        if (priority > 0)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean mediumPriority() {
+        if (priority == 0)
+            return true;
+        else
+            return false;
+    }
+
+    public boolean lowPriority() {
+        if (priority < 0)
+            return true;
+        else
+            return false;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public void setPerson(Person person) {
+        this.person = person;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
